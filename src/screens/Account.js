c0e69449e5c8b9022e03ec8c1d1c8c0f4fcbc15a
Gustavo-Valdez <86739150+Gustavo-Valdez@users.noreply.github.com>
2022-06-29 17:00:37 -0500

    Agregando archivo Account

---
+++ src/screens/Account.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export function Account() {
+  return (
+    <Vie>
+      <Text>Estamos en la screen de Account</Text>
+    </Vie>
+  );
+}
