b51ecc7c8482e7acd103bd9e3f6fe5db8d3f7a1e
Gustavo-Valdez <86739150+Gustavo-Valdez@users.noreply.github.com>
2022-06-29 16:54:19 -0500

    Agregando carpeta src, screens y archivp Restaurants

---
+++ src/screens/Restaurants.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export function Restaurants() {
+  return (
+    <view>
+      <Text>Extamos en la screen Restaurants</Text>
+    </view>
+  );
+}
