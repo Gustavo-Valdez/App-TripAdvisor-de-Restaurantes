d870747ccca1878c3adac67955c58fd3fd2d126c
Gustavo-Valdez <86739150+Gustavo-Valdez@users.noreply.github.com>
2022-06-29 17:12:59 -0500

    Agreagndo archivo Search, Ranking y actualizando nombres

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export function Account() {
+export function AccountScreen() {
   return (
     <Vie>
       <Text>Estamos en la screen de Account</Text>
--- src/screens/RestaurantsScreen.js
+++ src/screens/RestaurantsScreen.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export function Restaurants() {
+export function RestaurantsScreen() {
   return (
     <view>
       <Text>Extamos en la screen Restaurants</Text>
