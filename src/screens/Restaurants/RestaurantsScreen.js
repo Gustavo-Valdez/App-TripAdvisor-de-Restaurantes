4b30cad49ffcdbfa8ebf9a60a46273b4f0961f0d
Gustavo-Valdez <86739150+Gustavo-Valdez@users.noreply.github.com>
2022-06-30 18:07:07 -0500

    Navegación a screens

--- src/screens/Restaurants/RestaurantsScreen.js
+++ src/screens/Restaurants/RestaurantsScreen.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { Button } from "react-native-elements";
+import { screen } from "../../utils";
 
-export function RestaurantsScreen() {
+export function RestaurantsScreen(props) {
+  const { navigation } = props;
+
+  const goToAddRestaurant = () => {
+    //navigation.navigate(screen.restaurant.addRestaurant);
+
+    navigation.navigate(screen.account.tab, { screen: screen.account.account });
+  };
+
   return (
     <View>
       <Text>Estamos en la screen Restaurants</Text>
+
+      <Button title="Crear restaurante" onPress={goToAddRestaurant} />
     </View>
   );
 }
