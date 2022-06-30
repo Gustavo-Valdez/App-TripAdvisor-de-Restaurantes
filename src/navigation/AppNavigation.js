c2a9d3d4ec41127002d52f24bfbfc1c7d5d0b8ca
Gustavo-Valdez <86739150+Gustavo-Valdez@users.noreply.github.com>
2022-06-30 15:50:56 -0500

    Agregando if para hacer iconos

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -11,17 +11,10 @@ const Tab = createBottomTabNavigator();
 export function AppNavigation() {
   return (
     <Tab.Navigator
-      screenOptions={({ router }) => ({
+      screenOptions={({ route }) => ({
         tabBarActiveTintColor: "#00a680",
         tabBarInactiveTintColor: "#646464",
-        tabBarIcon: ({ color, size }) => (
-          <Icon
-            type="material-community"
-            name="home-outline"
-            color={color}
-            size={size}
-          />
-        ),
+        tabBarIcon: ({ color, size }) => screenOptions(route, color, size),
       })}
     >
       <Tab.Screen name="Restaurant" component={RestaurantsScreen} />
@@ -30,5 +23,33 @@ export function AppNavigation() {
       <Tab.Screen name="Search" component={SearchScreen} />
       <Tab.Screen name="Account" component={AccountScreen} />
     </Tab.Navigator>
+  );
+}
+
+function screenOptions(route, color, size) {
+  let iconName;
+
+  if (route.name === "Restaurant") {
+    iconName = "compass-outline";
+  }
+
+  if (route.name === "Favorites") {
+    iconName = "heart-outline";
+  }
+
+  if (route.name === "Ranking") {
+    iconName = "star-outline";
+  }
+
+  if (route.name === "Search") {
+    iconName = "magnify";
+  }
+
+  if (route.name === "Account") {
+    iconName = "home-outline";
+  }
+
+  return (
+    <Icon type="material-community" name={iconName} color={color} size={size} />
   );
 }
