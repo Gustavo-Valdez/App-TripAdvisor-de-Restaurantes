a82e0d54f80e3f3615f0a5a94aed1c05d10cc74f
Gustavo-Valdez <86739150+Gustavo-Valdez@users.noreply.github.com>
2022-06-30 11:46:40 -0500

    importando libreria y escribiendo codigo de iconos

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Icon } from "react-native-elements";
 import { RestaurantsScreen } from "../screens/RestaurantsScreen";
 import { FavoritesScreen } from "../screens/FavoritesScreen";
 import { RankingScreen } from "../screens/RankingScreen";
@@ -9,7 +10,20 @@ const Tab = createBottomTabNavigator();
 
 export function AppNavigation() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ router }) => ({
+        tabBarActiveTintColor: "#00a680",
+        tabBarInactiveTintColor: "#646464",
+        tabBarIcon: ({ color, size }) => (
+          <Icon
+            type="material-community"
+            name="home-outline"
+            color={color}
+            size={size}
+          />
+        ),
+      })}
+    >
       <Tab.Screen name="Restaurant" component={RestaurantsScreen} />
       <Tab.Screen name="Favorites" component={FavoritesScreen} />
       <Tab.Screen name="Ranking" component={RankingScreen} />
