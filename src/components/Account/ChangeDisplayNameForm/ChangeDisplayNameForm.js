40a3867afdb5856f9035b217c7290459aee31b18
Gustavo-Valdez <86739150+Gustavo-Valdez@users.noreply.github.com>
2022-07-04 17:16:43 -0500

    Controlando y validando formulario displayname

--- src/components/Account/ChangeDisplayNameForm/ChangeDisplayNameForm.js
+++ src/components/Account/ChangeDisplayNameForm/ChangeDisplayNameForm.js
@@ -1,9 +1,20 @@
 import React from "react";
 import { View } from "react-native";
 import { Input, Button } from "react-native-elements";
+import { useFormik } from "formik";
+import { initialValues, validationSchema } from "./ChangeDisplayNameForm.data";
 import { styles } from "./ChangeDisplayNameForm.styles";
 
 export function ChangeDisplayNameForm() {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validationSchema(),
+    validateOnChange: false,
+    onSubmit: (formValue) => {
+      console.log(formValue);
+    },
+  });
+
   return (
     <View style={styles.content}>
       <Input
@@ -13,11 +24,15 @@ export function ChangeDisplayNameForm() {
           name: "account-circle-outline",
           color: "#c2c2c2",
         }}
+        onChangeText={(text) => formik.setFieldValue("displayName", text)}
+        errorMessage={formik.errors.displayName}
       />
       <Button
         title="Cambiar nombre y apellidos"
         containerStyle={styles.btnContainer}
         buttonStyle={styles.btn}
+        onPress={formik.handleSubmit}
+        loading={formik.isSubmitting}
       />
     </View>
   );
