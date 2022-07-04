b07a1fa5cbf01ff57bac6d61fe823e3c3b8de1b9
Gustavo-Valdez <86739150+Gustavo-Valdez@users.noreply.github.com>
2022-07-03 20:38:38 -0500

    Opciones del usuario

--- src/screens/Account/UserLoggedScreen/UserLoggedScreen.js
+++ src/screens/Account/UserLoggedScreen/UserLoggedScreen.js
@@ -3,7 +3,7 @@ import { View } from "react-native";
 import { Button } from "react-native-elements";
 import { getAuth, signOut } from "firebase/auth";
 import { LoadingModal } from "../../../components";
-import { InfoUser } from "../../../components/Account";
+import { InfoUser, AccountOptions } from "../../../components/Account";
 import { styles } from "./UserLoggedScreen.styles";
 
 export function UserLoggedScreen() {
@@ -18,6 +18,8 @@ export function UserLoggedScreen() {
   return (
     <View>
       <InfoUser setLoading={setLoading} setLoadingText={setLoadingText} />
+
+      <AccountOptions />
 
       <Button
         title="Cerrar sesión"
