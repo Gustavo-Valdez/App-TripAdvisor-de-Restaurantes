448613b67a32108c7a6febf9ed13e6ca534b9f83
Gustavo-Valdez <86739150+Gustavo-Valdez@users.noreply.github.com>
2022-07-04 19:25:28 -0500

    Formulario cambiar email

--- src/components/Account/AccountOptions.js
+++ src/components/Account/AccountOptions.js
@@ -4,6 +4,7 @@ import { ListItem, Icon, Text } from "react-native-elements";
 import { map } from "lodash";
 import { Modal } from "../../components";
 import { ChangeDisplayNameForm } from "./ChangeDisplayNameForm";
+import { ChangeEmailForm } from "./ChangeEmailForm";
 
 export function AccountOptions(props) {
   const { onReload } = props;
@@ -21,7 +22,9 @@ export function AccountOptions(props) {
     }
 
     if (key === "email") {
-      setRenderComponent(<Text>Cambiar email</Text>);
+      setRenderComponent(
+        <ChangeEmailForm onClose={onCloseOpenModal} onReload={onReload} />
+      );
     }
 
     if (key === "password") {
