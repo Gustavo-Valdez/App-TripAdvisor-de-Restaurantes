266ccbfa4bdc0dd8d80bd241750846967bc3e47a
Gustavo-Valdez <86739150+Gustavo-Valdez@users.noreply.github.com>
2022-07-07 13:12:33 -0500

    Validando y controlando formulario cambio de contraseña

--- src/components/Account/ChangePasswordForm/ChangePasswordForm.js
+++ src/components/Account/ChangePasswordForm/ChangePasswordForm.js
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
 import { View } from "react-native";
 import { Input, Button } from "react-native-elements";
+import { useFormik } from "formik";
+import { initialValues, validationSchema } from "./ChangePasswordForm.data";
 import { styles } from "./ChangePasswordForm.styles";
 
 export function ChangePasswordForm() {
   const [showPassword, setShowPassword] = useState(false);
 
   const onShowPassword = () => setShowPassword((prevState) => !prevState);
+
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validationSchema(),
+    validateOnChange: false,
+    onSubmit: async (formValue) => {
+      console.log(formValue);
+    },
+  });
 
   return (
     <View style={styles.content}>
@@ -20,6 +31,8 @@ export function ChangePasswordForm() {
           color: "#c2c2c2",
           onPress: onShowPassword,
         }}
+        onChangeText={(text) => formik.setFieldValue("password", text)}
+        errorMessage={formik.errors.password}
       />
       <Input
         placeholder="Nueva contraseña"
@@ -31,6 +44,8 @@ export function ChangePasswordForm() {
           color: "#c2c2c2",
           onPress: onShowPassword,
         }}
+        onChangeText={(text) => formik.setFieldValue("newPassword", text)}
+        errorMessage={formik.errors.newPassword}
       />
       <Input
         placeholder="Repite nueva contraseña"
@@ -42,11 +57,17 @@ export function ChangePasswordForm() {
           color: "#c2c2c2",
           onPress: onShowPassword,
         }}
+        onChangeText={(text) =>
+          formik.setFieldValue("confirmNewPassword", text)
+        }
+        errorMessage={formik.errors.confirmNewPassword}
       />
       <Button
         title="Cambiar contraseña"
         containerStyle={styles.btnContainer}
         buttonStyle={styles.btn}
+        onPress={formik.handleSubmit}
+        loading={formik.isSubmitting}
       />
     </View>
   );
